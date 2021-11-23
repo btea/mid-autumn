b9ad6c3edc4ca7e048e9e21f8bab6dbb11aebe1e
btea <2356281422@qq.com>
2021-11-23 11:53:15 +0800

    feat: add fog

--- src/components/Moon.vue
+++ src/components/Moon.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="moon"></div>
+    <div class="moon">
+        <div class="fog"></div>
+    </div>
 </template>
-<style>
+<style lang="less">
 .moon {
     position: absolute;
     left: 50%;
@@ -11,5 +13,23 @@
     border-radius: 50%;
     background: #fff;
     box-shadow: 0 0 20px 20px rgba(255, 255, 255, 0.2);
+    overflow: hidden;
+    .fog{
+        width: 500px;
+        height: 200px;
+        background: radial-gradient(#000000ba, transparent);
+        position: relative;
+        top: -50px;
+        transform: translateX(-400px);
+        animation: move 20s linear infinite alternate;
+    }
+}
+@keyframes move {
+    0%{
+        transform: translateX(-400px);
+    }
+    100%{
+        transform: translateX(0);
+    }
 }
 </style>
