c5f86da5f33bf2b0aca1c01bea606e3b1b8d723e
btea <2356281422@qq.com>
2022-11-06 21:18:54 +0800

    chore: update festival

--- src/utils/getFestival.ts
+++ src/utils/getFestival.ts
@@ -1,8 +1,11 @@
 const festivalList: Record<string, string> = {
-  '01-01': 'NewYear',
-  '03-05': 'JingZhe',
-  '05-05': 'DuanWu',
-  '08-15': 'MidAutumn'
+  '2022-01-01': 'YuanDan',
+  '2022-01-31': 'ChuXi',
+  '2022-02-01': 'ChunJie',
+  '2022-03-05': 'JingZhe',
+  '2022-06-03': 'DuanWu',
+  '2022-09-10': 'MidAutumn',
+  '2022-10-01': 'GuoQin'
 }
 export function getFestival(): string {
   const date = getDateStr()
